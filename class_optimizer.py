09c28f14690fd6ecddbf8545281e4cab141255ee
unknown <hubingqing@htffundoffice.com>
2018-06-06 15:45:53 +0800

    6-6

--- class_optimizer.py
+++ class_optimizer.py
@@ -40,9 +40,9 @@ factor = pd.DataFrame(factor,index=factor_val.index,columns=factor_val.columns)
 
 # get data samples (when n >1, can get multiple samples)
 def get_chunk(timesteps,num_input,n=1,factor=factor):
-    x = np.zeros([n,timesteps,num_input])
+    x = np.zeros([timesteps,num_input])
     i = 0
-    while i < n:
+    while i < 1:
         # pick a day
         samples = np.random.randint(0,len(list(excess.index[0:-timesteps])),1)
         # current time point of index
@@ -52,23 +52,25 @@ def get_chunk(timesteps,num_input,n=1,factor=factor):
         # industry distribution
         current_industry = industry.iloc[current_comp_index,:]
         index_industry = [0]*len(industry_name)
+
         for j in range(0,len(industry_name)):
             current_j_industry = current_industry.loc[current_industry==j].index
             index_industry[j] = sum(comp[current_j_industry].dropna())
         index_industry = np.array(index_industry)/sum(index_industry)
+
         # pick num_input stocks(loc)
         stocks = random.sample(range(0,num_comp),num_input)
         # stocks and weights
         stocks = comp.index[stocks]
         weights = np.array(list(comp[stocks]))/100.0
         # history excess return
-        x[i] = excess.loc[excess.index[samples][0]:excess.index[samples+timesteps-1][0],stocks]
+        x = excess.loc[excess.index[samples][0]:excess.index[samples+timesteps-1][0],stocks]
         # history factor
         factor_h = factor.loc[excess.index[samples+timesteps-1][0],stocks]
 
 
-        if sum(sum(np.isnan(x[i])))>0:
-            i = i-1
+        #if sum(sum(np.isnan(x)))>0:
+            #i = i-1
         i = i+1
 
     return(x,weights,np.array(factor_h),np.array(current_industry[stocks]).astype('int32'),index_industry,stocks)
@@ -76,11 +78,12 @@ def get_chunk(timesteps,num_input,n=1,factor=factor):
 #
 class one_sample:
 
-    def __init__(self, num_input, from_index, timesteps, learning_rate, \
+    def __init__(self, num_input, num_final_stocks, from_index, timesteps, learning_rate, \
     training_steps,display_step, te_limit, industry_deviation, \
     turn_over_limit, weight_deviation, weight_max, num_industry):
         
         self.num_input = num_input
+        self.num_final_stocks = num_final_stocks
         self.from_index = from_index
         self.te_limit = te_limit
         self.industry_deviation = industry_deviation
@@ -98,39 +101,33 @@ class one_sample:
         # Graph related
         self.graph = tf.Graph()
         self.tf_train_samples = None
-        self.weight0 = None
         self.latest_weight = None
         self.factor = None
         self.index_industry = None
         self.industry_class = None
 
 
-    def define_graph(self):
-        
+    def define_graph:
+
         with self.graph.as_default():
-            self.tf_train_samples = tf.placeholder("float", [None, self.timesteps, self.num_input])
-            self.weight0 = tf.placeholder("float",self.num_input)
-            self.latest_weight = tf.placeholder("float",self.num_input)
-            self.factor = tf.placeholder('float',self.num_input)
-            self.index_industry = tf.placeholder('float',self.num_industry)
-            self.industry_class = tf.placeholder('int32',self.num_input)
-
-
-
-            weights = tf.Variable(tf.random_normal([1,self.num_input],0,0.01))
+            self.tf_train_samples = tf.placeholder("float",None)
+            self.latest_weight = tf.placeholder("float",None)
+            self.factor = tf.placeholder('float',None)
+            self.index_industry = tf.placeholder('float',None)
+            self.industry_class = tf.placeholder('int32',None)
+        
+            weights = tf.Variable(tf.random_normal([1,self.dim],0,0.01))
             
-
-            def model(reuse=None):
-
+            def model():
                 return weights + tf.expand_dims(self.latest_weight,0) 
-
+                
             # calculate loss
             def cal_loss(output,input_samples,factor):
                 self.prediction = tf.abs(output)
                 # the output stock weights after normalization
                 self.prediction = tf.expand_dims(tf.divide(self.prediction,tf.expand_dims(\
                 tf.reduce_sum(self.prediction,1),1)),1)
-                self.prediction_final = tf.reduce_sum(tf.multiply(input_samples,self.prediction),2)
+                self.prediction_final = tf.reduce_sum(tf.multiply(tf.expand_dims(input_samples,1),self.prediction),2)
                 factor = tf.expand_dims(factor,0)
                 self.fe = tf.reduce_sum(tf.reduce_sum(tf.multiply(\
                 tf.expand_dims(factor,0),self.prediction),2),1)
@@ -144,7 +141,7 @@ class one_sample:
                 if self.from_index == True else 0
                 self.turn_over = tf.reduce_mean(tf.divide(tf.abs(tf.subtract(self.prediction[0][0],\
                 self.latest_weight)),self.latest_weight))
-                loss = -self.fe + 100000*tf.nn.relu(self.te-self.te_limit) + \
+                loss = -1000*self.fe + 100000*tf.nn.relu(self.te-self.te_limit) + \
                 100000*tf.nn.relu(self.real_industry_deviation-self.industry_deviation) + \
                 100000*tf.nn.relu(real_weight_deviation-self.weight_deviation) + \
                 100000*tf.nn.relu(self.turn_over-self.turn_over_limit) + \
@@ -152,10 +149,11 @@ class one_sample:
                 self.ex_return = tf.reduce_sum(self.prediction_final)/self.timesteps*250
                 return loss
         
-            output =  model(self.tf_train_samples)
+            output =  model()
             self.loss = cal_loss(output=output,input_samples=self.tf_train_samples,factor=self.factor)
             self.optimizer = tf.train.GradientDescentOptimizer(learning_rate = self.learning_rate).minimize(self.loss)
-            
+
+    
 
     def run(self):
         
@@ -163,7 +161,8 @@ class one_sample:
         
         with self.session as sess:
             
-            # training
+            # first training
+            
             training_x, weight0, factor_h, industry_class, index_industry, stocks = get_chunk(self.timesteps,self.num_input)
             #latest_weight = np.array([1.0/self.num_input]*self.num_input)
             latest_weight = weight0/sum(weight0)
@@ -181,27 +180,71 @@ class one_sample:
                 _, l[step], ex_return[step], te[step], fe[step], w[step], turn_over[step], id_dev[step] = \
                 sess.run([self.optimizer,self.loss,self.ex_return,self.te,self.fe,self.prediction,\
                 self.turn_over,self.real_industry_deviation], \
-                                        feed_dict={self.tf_train_samples: training_x, self.weight0: weight0, \
+                                        feed_dict={self.tf_train_samples: training_x, \
                                         self.latest_weight:  latest_weight, self.factor: factor_h,\
-                                        self.industry_class: industry_class, self.index_industry: index_industry})       
+                                        self.industry_class: industry_class, self.index_industry: index_industry, \
+                                        })       
                 if step % self.display_step == 0:
                     print("Step {0:4s}, Loss = {1:12.3f}, Turn_over = {2:6.2f}, TE = {3:.3f}, Industry_dev = {4:.3f}, FE = {5:.3f}".format(str(step),l[step],turn_over[step],\
                     te[step],id_dev[step],fe[step]))
 
-            print("Optimization Finished!")
+            print("First Optimization Finished!")
+
+            result = pd.DataFrame([w[-1],latest_weight,industry_class,factor_h],index=['final_weight','initial_weight', \
+            'industry','BP'],columns=stocks).T
+            print(result.sort_values(by='final_weight'))
+
+            # choose final stocks
+            stocks = result.sort_values(by='final_weight',ascending=False).iloc[0:self.num_final_stocks].index
+            d = dict(list(zip(result.index,list(range(0,result.shape[0])))))
+            stocks_location = [d.get(s) for s in stocks]
+
+            # new inputs
+            weight0 = weight0[stocks_location]
+            latest_weight = weight0/sum(weight0)
+            training_x = pd.DataFrame(training_x)
+            training_x = training_x.iloc[:,stocks_location]
+            factor_h = factor_h[stocks_location]
+            industry_class = industry_class[stocks_location]
+
+        
+            # second training
+            
+            tf.global_variables_initializer().run()
+            print('Start Training:')
+            l = np.zeros(self.training_steps)
+            ex_return = np.zeros(self.training_steps)
+            te = np.zeros(self.training_steps)
+            fe = np.zeros(self.training_steps)
+            w = np.zeros([self.training_steps,self.num_input])
+            turn_over = np.zeros(self.training_steps)
+            id_dev = np.zeros(self.training_steps)
+            for step in range(0, self.training_steps):
+            # Run optimization op (backprop)
+                _, l[step], ex_return[step], te[step], fe[step], w[step], turn_over[step], id_dev[step] = \
+                sess.run([self.optimizer,self.loss,self.ex_return,self.te,self.fe,self.prediction,\
+                self.turn_over,self.real_industry_deviation], \
+                                        feed_dict={self.tf_train_samples: training_x, \
+                                        self.latest_weight:  latest_weight, self.factor: factor_h,\
+                                        self.industry_class: industry_class, self.index_industry: index_industry, \
+                                        })       
+                if step % self.display_step == 0:
+                    print("Step {0:4s}, Loss = {1:12.3f}, Turn_over = {2:6.2f}, TE = {3:.3f}, Industry_dev = {4:.3f}, FE = {5:.3f}".format(str(step),l[step],turn_over[step],\
+                    te[step],id_dev[step],fe[step]))
+
+            print("Second Optimization Finished!")
 
             self.fe_end = fe[-1]
             result = pd.DataFrame([w[-1],latest_weight,industry_class,factor_h],index=['final_weight','initial_weight', \
             'industry','BP'],columns=stocks).T
             print(result)
             
-            
 
 if __name__ == '__main__':
     num_exp = 1
-    net = one_sample(num_input = num_comp, from_index=True, timesteps = 100, learning_rate = 0.00001, \
+    net = one_sample(num_input = num_comp, num_final_stocks = 50, from_index=True, timesteps = 100, learning_rate = 0.00001, \
     training_steps = 2000, display_step = 100, te_limit = 0.05, industry_deviation = 0.01, \
-    turn_over_limit = 0.1, weight_deviation = 0.03, weight_max = 0.1, num_industry = len(industry_name))
+    turn_over_limit = 0.5, weight_deviation = 0.03, weight_max = 0.1, num_industry = len(industry_name))
     net.define_graph()
     fe_end = 0
     for i in range(0,num_exp):
